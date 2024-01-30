a8aa28938c4f229a6e9b88bae0a8fba5386abc96
ridsor <97663231+ridsor@users.noreply.github.com>
2024-01-31 03:06:34 +0900

    Feature/account (#4)
    * membuat halaman login
    
    * membuat halaman dashboard
    
    * membuat halaman account

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Aside from "@/components/layouts/User/Aside";
+import { usePathname } from "next/navigation";
+import { createContext, useState } from "react";
+
+const enableNavbar = ["/dashboard", "/profile", "/account"];
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const [isSidebar, setSidebar] = useState<boolean>(true);
+
+  return (
+    <>
+      <main
+        className={`${
+          isSidebar ? "md:left-[250px] md:w-[calc(100%-250px)]" : ""
+        } left-[60px] w-[calc(100%-60px)] relative bg-three min-h-screen h-fit`}
+      >
+        {children}
+      </main>
+      {enableNavbar.includes(pathname) && (
+        <Aside isSidebar={isSidebar} setSidebar={setSidebar} />
+      )}
+    </>
+  );
+}
---
+++ src/components/layouts/User/Aside.tsx
@@ -0,0 +1,162 @@
+import { TbLayoutDashboard } from "react-icons/tb";
+import { FaShoppingCart } from "react-icons/fa";
+import { RiShieldUserFill } from "react-icons/ri";
+import { FaPlay } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa6";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import user_img from "@/assets/img/user-img.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+interface Props {
+  isSidebar: boolean;
+  setSidebar: (value: boolean) => void;
+}
+
+export default function Aside({ isSidebar, setSidebar }: Props) {
+  return (
+    <aside
+      className={`${
+        isSidebar ? "right-0" : ""
+      } fixed top-0 bottom-0 left-0 md:right-auto`}
+    >
+      <div className="relative w-full h-full">
+        {isSidebar ? (
+          <div
+            className="absolute w-full h-full bg-[rgba(0,0,0,0.2)] md:hidden"
+            onClick={() => setSidebar(false)}
+          ></div>
+        ) : (
+          ""
+        )}
+        <div
+          className={`${
+            isSidebar ? " w-[250px]" : "w-fit"
+          } h-screen min-h-fit relative z-50`}
+        >
+          <div
+            className={`${
+              isSidebar ? "right-3 top-3" : "-right-5 top-11"
+            } absolute bg-three z-10 rounded-full p-1 flex items-center justify-center`}
+          >
+            <button
+              className="rounded-full bg-two text-three active:ring active:ring-[rgba(0,0,0,.1)]"
+              onClick={() => setSidebar(!isSidebar)}
+            >
+              <div className="p-2">
+                <FaPlay
+                  className={`${
+                    isSidebar ? "-rotate-180" : ""
+                  } text-lg transition`}
+                />
+              </div>
+            </button>
+          </div>
+          <article
+            className={`bg-[#E4EEDD] w-full h-full grid px-2 py-4 overflow-auto rounded-tr-2xl rounded-br-2xl`}
+          >
+            <div>
+              <div className="brand flex items-center mb-3">
+                <div className={`p-1.5`}>
+                  <img
+                    src={"/sumi-tailor.svg"}
+                    alt="logo"
+                    className="h-8 aspect-square"
+                  />
+                </div>
+                {isSidebar ? (
+                  <span className="font-semibold text-base font-one">
+                    Sumi Tailor
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <hr className="border-top border-[#d7d3cc] mb-3" />
+              <div className={`${isSidebar ? "px-2" : ""} user-profile mb-2`}>
+                <div
+                  className={`${
+                    isSidebar ? "w-[70px]" : "w-[44px]"
+                  } user-img aspect-square mx-auto mb-1`}
+                >
+                  <Image
+                    src={user_img}
+                    alt="user_img"
+                    className="w-full h-full object-cover object-center rounded-full shadow-sm border-2 border-[#d7d3cc]"
+                  />
+                </div>
+                {isSidebar ? (
+                  <>
+                    <div className="user-name text-center font-semibold">
+                      Admin
+                    </div>
+                  </>
+                ) : (
+                  ""
+                )}
+                <Link
+                  href="/profile"
+                  className="flex items-center hover:bg-four rounded-md w-full font-semibold active:ring active:ring-three mt-2"
+                >
+                  <div className="p-3">
+                    <FaUser className="text-xl" />
+                  </div>
+                  {isSidebar ? "Profil" : ""}
+                </Link>
+              </div>
+              <hr className="border-top border-[#d7d3cc]" />
+              <ul
+                className={`${
+                  isSidebar ? "p-2" : "py-2"
+                } flex flex-col font-semibold`}
+              >
+                <li>
+                  <Link
+                    href="/dashboard"
+                    className="flex items-center hover:bg-four rounded-md active:ring active:ring-three"
+                  >
+                    <div className="p-3">
+                      <TbLayoutDashboard className="text-xl" />
+                    </div>
+                    {isSidebar ? "Dashboard" : ""}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/orders"
+                    className="flex items-center hover:bg-four rounded-md active:ring active:ring-three"
+                  >
+                    <div className="p-3">
+                      <FaShoppingCart className="text-xl" />
+                    </div>
+                    {isSidebar ? "Pesanan" : ""}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/account"
+                    className="flex items-center hover:bg-four rounded-md active:ring active:ring-three"
+                  >
+                    <div className="p-3">
+                      <RiShieldUserFill className="text-xl" />
+                    </div>
+                    {isSidebar ? "Account" : ""}
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="self-end">
+              <hr className="border-top border-[#d7d3cc] mb-2" />
+              <button className="flex items-center hover:bg-four rounded-md w-full font-semibold active:ring active:ring-three">
+                <div className="p-3">
+                  <FaArrowRightFromBracket className="text-xl" />
+                </div>
+                {isSidebar ? "Sign Out" : ""}
+              </button>
+            </div>
+          </article>
+        </div>
+      </div>
+    </aside>
+  );
+}
