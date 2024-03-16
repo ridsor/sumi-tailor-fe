a88399a8c36e2f401b184e650389c26c3d8e2d7e
ridsor <ryansyukur.it@gmail.com>
2024-03-16 20:41:52 +0700

    memperbarui revalidate fetch dan mengubah desain halaman profile

--- src/app/(admin)/profile/page.tsx
+++ src/app/(admin)/profile/page.tsx
@@ -19,27 +19,32 @@ const ProfilePage = () => {
             </h1>
             <form method="post">
               <div className="flex items-center gap-4 mb-4">
-                <div className="w-24 rounded-full aspect-square overflow-hidden">
+                <label
+                  htmlFor="image"
+                  className="w-24 rounded-full aspect-square overflow-hidden cursor-pointer">
                   <Image
                     src={user_img}
                     alt=""
                     className="w-full h-full"
                     priority
                   />
-                </div>
-                <button className="px-3 py-2 bg-one text-white rounded-md font-semibold hover:bg-[rgba(72,60,50,.5)] focus:ring focus:ring-[rgba(72,60,50,.5)]">
-                  Upload
-                </button>
-                <button className="px-3 py-2 bg-gray-200 text-one rounded-md font-semibold hover:bg-gray-300 focus:ring focus:ring-[rgba(209,213,219,.5)]">
-                  Hapus
+                </label>
+                <input
+                  type="file"
+                  name="image"
+                  id="image"
+                  hidden
+                  accept="image/png, image/jpeg, image/jpg"
+                />
+                <button
+                  type="button"
+                  onClick={() => setIsChangePassword((prev) => !prev)}
+                  className="px-3 mb-4 py-2 bg-gray-200 text-one rounded-md font-semibold hover:bg-gray-300 focus:ring focus:ring-[rgba(209,213,219,.5)]">
+                  {isChangePassword ? "Ubah Detail" : "Ubah Password"}
                 </button>
               </div>
             </form>
-            <button
-              onClick={() => setIsChangePassword((prev) => !prev)}
-              className="px-3 mb-4 py-2 bg-gray-200 text-one rounded-md font-semibold hover:bg-gray-300 focus:ring focus:ring-[rgba(209,213,219,.5)]">
-              {isChangePassword ? "Ubah Detail" : "Ubah Password"}
-            </button>
+
             {isChangePassword ? <FormProfilPassword /> : <FormProfilDetail />}
           </div>
         </div>
