bbba1cc71660af3c4c05eeb4e39326b04430ed34
ridsor <ryansyukur.it@gmail.com>
2024-05-19 18:07:00 +0900

    memperbarui desain  home, about, galeri, service

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -30,9 +30,18 @@ export const metadata: Metadata = {
 const GalleryPage = () => {
   return (
     <main>
-      <section className="bg-[#E4EEDD]">
+      <section className="bg-[#E4EEDD] py-36">
         <div className="container px-4">
-          <div className="pt-36 pb-16">
+          <div>
+            <h2 className="mb-2 text-3xl text-center font-pt-serif">
+              Galeri Karya Jahitan Kami
+            </h2>
+            <p className="mb-8 text-center">
+              Telusuri berbagai karya jahitan eksklusif yang telah kami ciptakan
+              dengan dedikasi. Setiap potong pakaian adalah hasil dari keahlian
+              tinggi dan perhatian pada detail, dibuat khusus untuk memberikan
+              sentuhan elegan dan kenyamanan bagi Anda.
+            </p>
             <GalleryList />
           </div>
         </div>
