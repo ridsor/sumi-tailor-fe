ad660b0bff24f863d9979781cfd5a2452ff154a4
ridsor <97663231+ridsor@users.noreply.github.com>
2024-06-01 14:35:59 +0900

    memperbaiki bug create order

--- src/app/(admin)/orders/OrderInput.tsx
+++ src/app/(admin)/orders/OrderInput.tsx
@@ -237,7 +237,6 @@ export default function OrderInput() {
       formData.append("image", inputs.image as File);
 
       const createResponse = await createOrder(formData);
-      console.log(createResponse);
       if (createResponse?.status != "success") {
         if (typeof createResponse?.errors.no_hp != "undefined") {
           setValidate((prev) => ({
