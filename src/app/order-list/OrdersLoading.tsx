29d7e378050748fcffcff22b75962dfa32eaace0
ridsor <ryansyukur.it@gmail.com>
2024-06-02 23:59:20 +0900

    memperbaiki desain loading order

--- src/app/order-list/OrdersLoading.tsx
+++ src/app/order-list/OrdersLoading.tsx
@@ -1,11 +1,11 @@
 export default function OrdersLoading() {
   return (
     <>
-      <div className="flex justify-between  mb-7 w-fit gap-3">
+      <div className="flex justify-between  mb-7 w-fit gap-3 animate-pulse">
         <div className=" rounded-md h-[30px] bg-[#bdc3c7] w-[71px]  " />
         <div className=" rounded-md h-[30px] bg-[#bdc3c7] w-[71px]" />
       </div>
-      <div className="orders flex flex-col gap-6 w-full pb-10 p-1 animate-pulse">
+      <div className="orders flex flex-col gap-6 w-full pb-10 p-1">
         <div className="columns-[350px] lg:columns-2 gap-3">
           <div className="bg-[#bdc3c7] h-[86px] rounded-md mb-3"></div>
           <div className="bg-[#bdc3c7] h-[86px] rounded-md mb-3"></div>
