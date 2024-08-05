6f358ab3bf578f338114dd48b80e69e6654b9e3d
Ryan Syukur <ryansyukur.it@gmail.com>
2024-08-05 19:32:31 +0900

    fix responsive text

--- src/app/order-list/page.tsx
+++ src/app/order-list/page.tsx
@@ -10,12 +10,14 @@ export default async function OrdersPage({
 }) {
   return (
     <main>
-      <section className="pt-[125px] mb-[125px]">
-        <div className="container">
-          <article className="px-4">
-            <div className="relative">
-              <h2 className="text-5xl font-bold mb-12 text-background lg:text-one text-center lg:text-left">Daftar Pesanan</h2>
-              <div className="flex sm:items-center mb-3 gap-3 flex-col sm:flex-row justify-between">
+      <section className='pt-[125px] mb-[125px]'>
+        <div className='container'>
+          <article className='px-4'>
+            <div className='relative'>
+              <h2 className='text-3xl md:text-4xl lg:5xl font-bold mb-12 text-background lg:text-one text-center lg:text-left'>
+                Daftar Pesanan
+              </h2>
+              <div className='flex sm:items-center mb-3 gap-3 flex-col sm:flex-row justify-between'>
                 <OrderSearch />
               </div>
               <Suspense fallback={<OrdersLoading />}>
